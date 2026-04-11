79524f22de70fc50cbce24241c90f2ab6441c8f6
bsowjanya861-dot <bsowjanya861@gmail.com>
2026-04-11 21:49:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,27 +3,17 @@ from predict import predict_image
 from PIL import Image
 import tempfile
 
-st.title("🧠 MRI Brain Stroke Detection")
-st.write("Upload MRI image to predict stroke type")
+st.title("🧠 Brain Stroke Classification")
 
-uploaded_file = st.file_uploader("Upload MRI Image", type=["jpg", "png", "jpeg"])
+uploaded_file = st.file_uploader("Upload MRI Image", type=["jpg","png","jpeg"])
 
 if uploaded_file is not None:
-    
-    # Show image
-    st.image(uploaded_file, caption="Uploaded Image", use_container_width=True)
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Uploaded Image", use_column_width=True)
 
-    # Save temp file
-    with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
-        tmp_file.write(uploaded_file.read())
-        temp_path = tmp_file.name
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".jpg") as tmp:
+        image.save(tmp.name, format="JPEG")
 
-    # Predict
-    result, probs, classes = predict_image(temp_path)
+        prediction, confidence = predict_image(tmp.name)
 
-    st.subheader("Prediction:")
-    st.success(result)
-
-    st.subheader("Confidence:")
-    for i, p in enumerate(probs):
-        st.write(f"{classes[i]}: {p:.2f}")
+    st.success(f"Prediction: {prediction}")
