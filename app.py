3be4d6b5b950f0aef8802dccead491d3663097e8
bsowjanya861-dot <bsowjanya861@gmail.com>
2026-04-13 13:14:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,24 +5,6 @@ import tempfile
 
 st.title("🧠 Brain Stroke Prediction")
 
-# Background
-def set_background():
-    st.markdown(
-        """
-        <style>
-        .stApp {
-            background-color: #E6E6FA !important;
-        }
-        html, body {
-            background-color: #E6E6FA !important;
-        }
-        </style>
-        """,
-        unsafe_allow_html=True
-    )
-
-set_background()
-
 uploaded_file = st.file_uploader("Upload MRI Image", type=["jpg", "png", "jpeg"])
 
 if uploaded_file is not None:
@@ -31,9 +13,5 @@ if uploaded_file is not None:
 
     with tempfile.NamedTemporaryFile(delete=False, suffix=".jpg") as tmp:
         image.save(tmp.name, format="JPEG")
-        temp_path = tmp.name
-
-    if st.button("🔍 Predict"):
-        prediction, confidence = predict_image(temp_path)
-        st.success(f"Prediction: {prediction}")
-        st.info(f"Confidence: {confidence:.2f}")
+        prediction, confidence = predict_image(tmp.name) 
+    st.success(f"Prediction: {prediction}")
